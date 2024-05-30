b4f9d4d641c940855c595399818375f42c196b41
kriswind <126849955+kriswind@users.noreply.github.com>
2024-05-30 15:04:33 +0200

    added julia support

--- init.lua
+++ init.lua
@@ -656,7 +656,7 @@ require('lazy').setup({
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     opts = {
-      ensure_installed = { 'bash', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc', 'rust', 'javascript', 'go' },
+      ensure_installed = { 'bash', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc', 'rust', 'javascript', 'go', 'julia' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
